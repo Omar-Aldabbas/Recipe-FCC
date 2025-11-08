f9304bf0a0735a0c6c3227bdb9ba8b0ccfad5d0a
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-11-08 10:26:39 -0800

    9:10

--- src/components/PadComp.jsx
+++ src/components/PadComp.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import {cn} from "./cn"
 export const Pad = (props) => {
-    const [isOn, setIsOn] =useState(props.on)
+    // const [isOn, setIsOn] =useState(props.on)
 
-    const handleClick = () => {
-        setIsOn(prev => !prev)
-    }
+
   return (
     <button
-    onClick={handleClick}
+      onClick={() => props.toggle(props.id)}
       key={props.id}
       style={{ backgroundColor: props.color }}
-      className={cn("w-36 h-36 border-2- border-gray-800 border-4 rounded-xl ", isOn ? "opacity-100" : "opacity-10")}
+      className={cn("w-36 h-36 border-2- border-gray-800 border-4 rounded-xl ", props.on ? "opacity-100" : "opacity-10")}
     >
     </button>
   );
--- src/components/PadsGrid.jsx
+++ src/components/PadsGrid.jsx
@@ -5,14 +5,24 @@ import { Pad } from "./PadComp";
 export const Pads = () => {
   const [isPad, setIsPad] = useState(pads);
 
-  const toggle = () => {
-    console.log("Clicked")
-  }
+  const toggle = (id) => {
+    console.log(id);
+
+    setIsPad((prevPads) =>
+      prevPads.map((pad) => (pad.id === id ? { ...pad, on: !pad.on } : pad))
+    );
+  };
 
   return (
     <div className="min-h-screen bg-slate-200  p-12 space-x-2 space-y-2 ">
       {isPad.map((pad) => (
-        <Pad key={pad.id} color={pad.color} on={pad.on} toggle={toggle}/>
+        <Pad
+          key={pad.id}
+          id={pad.id}
+          color={pad.color}
+          on={pad.on}
+          toggle={toggle}
+        />
       ))}
     </div>
   );
